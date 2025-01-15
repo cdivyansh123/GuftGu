d633e886eaa582fc4a8314f564d5b0071e1423b6
cdivyansh <cdivyansh470@gmail.com>
2025-01-15 18:25:24 +0530

    GuftGu initial commit

---
+++ app/src/main/java/com/example/guftgu/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.guftgu
+
+object Constants {
+    val apiKey="AIzaSyAgF2bbdCkXIM7Vm_BW_Pm4SlypevA0B-M"
+}
---
+++ app/src/main/java/com/example/guftgu/MessageModel.kt
@@ -0,0 +1,6 @@
+package com.example.guftgu
+
+data class MessageModel(
+    val message:String,
+    val role:String,
+)
